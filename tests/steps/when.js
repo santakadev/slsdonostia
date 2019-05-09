6a7572bd6b5bb6caa9d3b7eb4e58a9993d40b59f
Daniel Santamaría <santaka87@gmail.com>
2019-05-09 12:16:27 +0200

    add integration test

--- tests/steps/when.js
+++ tests/steps/when.js
@@ -1,3 +1,5 @@
+const http = require("superagent-promise")(require("superagent"), Promise);
+
 const viaHandler = async function(name, event) {
     const handler = require(`../../src/functions/${name}`)
     const response = await handler.handler(event);
@@ -5,7 +7,36 @@ const viaHandler = async function(name, event) {
     return response;
 }
 
+const viaHttp = async function(functionPath) {
+    const apiRoot = "https://k80wvhzlna.execute-api.eu-west-1.amazonaws.com/dev/api";
+    const method = "GET";
+
+    const url = `${apiRoot}/${functionPath}`;
+
+    console.log(url);
+
+    try {
+        const httpReq = http(method, url);
+        const res = await httpReq;
+
+        return {
+            statusCode: res.status,
+            body: res.body
+        };
+    } catch (err) {
+        if (err.status) {
+            return {
+                statusCode: err.status
+            };
+        }
+        throw err;
+}
+}
+
 module.exports.we_invoke_helloWorld = (name) => {
     const event = { pathParameters: { name: name } };
-    return viaHandler('helloWorld', event);
+
+    return process.env.TEST_MODE === 'http' ?
+        viaHttp(`helloWorld/${name}`) :
+        viaHandler('helloWorld', event);
 }

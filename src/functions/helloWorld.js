a7b9ee8c188a6c2a7fc57852e570ea2a4db5f378
Daniel Santamaría <santaka87@gmail.com>
2019-05-09 10:37:13 +0200

    use path parameter instead of query string

--- src/functions/helloWorld.js
+++ src/functions/helloWorld.js
@@ -1,5 +1,5 @@
 exports.handler = async (event) => {
-    const { name } = event.queryStringParameters;
+    const { name } = event.pathParameters;
 
     const response = {
         statusCode: 200,
